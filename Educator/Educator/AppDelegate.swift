895565e5813df984350c6342595b0ef2e067a658
Jake MacMullin <jake@stripysock.com.au>
2017-04-23 13:47:32 +1000

    Commit initial empty project

---
+++ Educator/Educator/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Educator
+//
+//  Created by Jake MacMullin on 23/4/17.
+//  Copyright © 2017 Early Learning STEM Australia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
